36746ae5d67cd7ba2f9e0d17a4a5a23107520322
Pragati Agarwal <pragatinov@gmail.com>
2025-07-26 16:02:34 +0530

    Added the popular residencies slider below

--- realestate/src/App.js
+++ realestate/src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
+import Companies from './Components/Companies/Companies';
 import Extra from './Components/Extra/Extra';
 import Header from './Components/Header/Header';
+import Residencies from './Components/Residencies/Residencies';
 function App() {
   return (
     <div className="App">
+      <div>
       <Header/>
       <Extra/>
+      </div>
+      <Companies/>
+      <Residencies/>
     </div>
+
   );
 }
 
